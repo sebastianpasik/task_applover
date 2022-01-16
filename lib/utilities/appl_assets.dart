6896cf30136fc58c995e3ceaf2f9de36997881d6
Inspironn1525 <pasik.sebastian@gmail.com>
2022-01-16 12:54:16 +0000

    [dev] Add assets and add libraries

---
+++ lib/utilities/appl_assets.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/cupertino.dart';
+
+abstract class ApplAsset {
+  ApplAsset._();
+
+  static const applover_logo = 'assets/applover.svg';
+}
---
+++ lib/utilities/appl_colors.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/cupertino.dart';
+
+abstract class ApplColors {
+  ApplColors._();
+
+  static const background = Color(0xFF534C5F);
+  static const grey = Color(0xFF000029);
+  static const green = Color(0xFF1DE278);
+}
